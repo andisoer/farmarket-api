6b8de914c0e6aa80619ffadb73798c40ecdacaf7
Andi Surya <andi502.as@gmail.com>
2024-06-04 09:13:46 +0700

    (feat): post vegetables

--- routes/vegetables.js
+++ routes/vegetables.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
+
+const multer = require('multer');
 const vegetableHandler = require('../handler/vegetable_handler.js');
 
-/* GET vegetables listing. */
-router.get('/', async function (req, res, next) {
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Directory to save uploaded files
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // File name
+    }
+});
+const upload = multer({ storage: storage });
+
+/* GET Vegetables */
+router.get('/', async (req, res, next) => {
     try {
-        res.json(await vegetableHandler.getMultiple(req.query.page));
+        res.json(await vegetableHandler.getAll(req.query.page));
+    } catch (err) {
+        console.error(`Error while getting programming languages `, err.message);
+        next(err);
+    }
+});
+
+/* POST Vegetable */
+router.post('/', async (req, res, next) => {
+    try {
+        const response = await vegetableHandler.insertVegetable(req)
+
+        res.status(401).json(response);
     } catch (err) {
         console.error(`Error while getting programming languages `, err.message);
         next(err);
--- services/config.js
+++ services/config.js
@@ -1,10 +1,10 @@
 const config = {
     db: {
         /* don't expose password or any sensitive info, done only for demo */
-        host: "db4free.net",
-        user: "restapitest123",
-        password: "restapitest123",
-        database: "restapitest123",
+        host: "localhost",
+        user: "root",
+        password: "1234567890",
+        database: "farmarket_db",
         connectTimeout: 60000
     },
     listPerPage: 10,
