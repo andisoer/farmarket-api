b1c84961ad3f511158f726f49a4b563c8e6e0f32
Andi Surya <andi502.as@gmail.com>
2024-06-17 23:56:23 +0700

    (config): adjust mysql config

--- services/config.js
+++ services/config.js
@@ -1,11 +1,12 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: 'localhost',
+    host: 'farmarket-db.c3em8uegcnl8.ap-southeast-1.rds.amazonaws.com',
+    port: '3306',
     user: 'root',
     password: '1234567890',
     database: 'farmarket_db',
-    connectTimeout: 60000,
+    connectTimeout: 10000,
   },
   listPerPage: 10,
 };
