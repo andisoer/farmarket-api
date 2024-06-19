890a068ebd02b87fc64699a870d2fb70990c0118
Andi Surya <andi502.as@gmail.com>
2024-06-19 22:26:49 +0700

    config database

--- services/config.js
+++ services/config.js
@@ -2,7 +2,8 @@ const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
     // host: 'farmarket-db.c3em8uegcnl8.ap-southeast-1.rds.amazonaws.com',
-    host: 'localhost',
+    // host: 'localhost',
+    host: '170.187.228.163',
     // host: 'localhost',
     port: '3306',
     user: 'digiappc_farmarket_user',
